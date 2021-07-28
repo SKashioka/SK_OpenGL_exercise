91f6846bdaf2079b6e3f65d44b024f3083ee9fcd
Kashioka Shuya <kashioka.shuya@ac.jaxa.jp>
2021-07-28 21:07:30 +0900

    red triangle

---
+++ common/shader.hpp
@@ -0,0 +1,13 @@
+//
+//  shader.hpp
+//  GL_triangle
+//
+//  Created by 柏岡秀哉 on 2021/07/28.
+//
+
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
