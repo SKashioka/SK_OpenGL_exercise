5566f573fac8b3fb30ff177e1373896ba8c1c3e8
Kashioka Shuya <kashioka.shuya@ac.jaxa.jp>
2021-08-10 15:21:29 +0900

    20210810 mouse and keyboard

---
+++ GL_Keyboard_Mouse/GL_Keyboard_Mouse/main.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  GL_Keyboard_Mouse
+//
+//  Created by 柏岡秀哉 on 2021/08/10.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include "shader.hpp"
+#include "texture.hpp"
+#include "controls.hpp"
+
+int main(void){
+    
+    // Initialize GLFW
+    if(!glfwInit()){
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Open a window and create its openGL context
+    window = glfwCreateWindow(512,512, "keybord and mouse", NULL, NULL);
+    if(window == NULL){
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    // Initialize GLEW
+    glewExperimental = true;
+    if(glewInit() != GLEW_OK){
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // Hide the mouse and enable unlimited mouvement
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    //
+    
+    
+}
