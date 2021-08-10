97f2af556d87643aff131da23b4384ebf8231ec8
Kashioka Shuya <kashioka.shuya@ac.jaxa.jp>
2021-08-10 18:36:26 +0900

    20210810 mouse and keyboard add rotation

--- GL_Keyboard_Mouse/GL_Keyboard_Mouse/main.cpp
+++ GL_Keyboard_Mouse/GL_Keyboard_Mouse/main.cpp
@@ -37,7 +37,7 @@ int main(void){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     // Open a window and create its openGL context
-    window = glfwCreateWindow(512,512, "keybord and mouse", NULL, NULL);
+    window = glfwCreateWindow(1024,1024, "keybord and mouse", NULL, NULL);
     if(window == NULL){
         fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
         getchar();
@@ -60,7 +60,196 @@ int main(void){
     // Hide the mouse and enable unlimited mouvement
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     
-    //
+    // Set the mous at the center of the creen
+    glfwPollEvents();
+    glfwSetCursorPos(window, 512/2, 512/2);
     
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
     
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    
+    // Accept fragment if it closer to the cammera than former one
+    glDepthFunc(GL_LESS);
+    
+    // Cull triangles which normal is not toward the camera
+    glEnable(GL_CULL_FACE);
+    
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    
+    // Create and compile our GLSL program from shaders
+    GLuint programID = LoadShaders("/Users/shuya/SKashi/OpenGL/GL_exercise/GL_Keyboard_Mouse/GL_Keyboard_Mouse/TransformVertexShader.vertexshader", "/Users/shuya/SKashi/OpenGL/GL_exercise/GL_Keyboard_Mouse/GL_Keyboard_Mouse/TextureFragmentShader.fragmentshader");
+    
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    
+    // Load the texture
+    GLuint Texture = loadDDS("/Users/shuya/SKashi/OpenGL/GL_exercise/GL_Keyboard_Mouse/GL_Keyboard_Mouse/uvtemplate.DDS");
+    
+    GLuint TextureID = glGetUniformLocation(programID, "myTextureSample");
+    
+    // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+         1.0f, 1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+         1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+         1.0f,-1.0f,-1.0f,
+         1.0f, 1.0f,-1.0f,
+         1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+         1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+         1.0f,-1.0f, 1.0f,
+         1.0f, 1.0f, 1.0f,
+         1.0f,-1.0f,-1.0f,
+         1.0f, 1.0f,-1.0f,
+         1.0f,-1.0f,-1.0f,
+         1.0f, 1.0f, 1.0f,
+         1.0f,-1.0f, 1.0f,
+         1.0f, 1.0f, 1.0f,
+         1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+         1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+         1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+         1.0f,-1.0f, 1.0f
+    };
+
+    // Two UV coordinatesfor each vertex. They were created with Blender.
+    static const GLfloat g_uv_buffer_data[] = {
+        0.000059f, 0.000004f,
+        0.000103f, 0.336048f,
+        0.335973f, 0.335903f,
+        1.000023f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.336024f, 0.671877f,
+        0.667969f, 0.671889f,
+        1.000023f, 0.000013f,
+        0.668104f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.000059f, 0.000004f,
+        0.335973f, 0.335903f,
+        0.336098f, 0.000071f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.336024f, 0.671877f,
+        1.000004f, 0.671847f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.668104f, 0.000013f,
+        0.335973f, 0.335903f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.668104f, 0.000013f,
+        0.336098f, 0.000071f,
+        0.000103f, 0.336048f,
+        0.000004f, 0.671870f,
+        0.336024f, 0.671877f,
+        0.000103f, 0.336048f,
+        0.336024f, 0.671877f,
+        0.335973f, 0.335903f,
+        0.667969f, 0.671889f,
+        1.000004f, 0.671847f,
+        0.667979f, 0.335851f
+    };
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    GLuint uvbuffer;
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+    
+    do{
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // Use our shader
+        glUseProgram(programID);
+        
+        // Compute the MVP matrix from keyboard and mouse input
+        computeMatricesFromInputs();
+        glm::mat4 ProjectionMatrix = getProjectionMatrix();
+        glm::mat4 ViewMatrix = getViewMatrix();
+        glm::mat4 ModelMatrix = glm::mat4(1.0);
+        glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+        
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        
+        // Bind Our texture in Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        // Set our "myTexturSampler" sampler to use Texture Uint 0
+        glUniform1i(TextureID, 0);
+        
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                              0,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              0,
+                              (void*)0
+                              );
+        
+        //2nd attribute buffer :UVs
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(
+                              1,
+                              2,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              0,
+                              (void*)0
+                              );
+        
+        //Draw triangle
+        glDrawArrays(GL_TRIANGLES, 0, 12*3);
+        
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    } while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+            glfwWindowShouldClose(window) == 0 );
+
+     // Cleanup VBO and shader
+     glDeleteBuffers(1, &vertexbuffer);
+     glDeleteBuffers(1, &uvbuffer);
+     glDeleteProgram(programID);
+     glDeleteTextures(1, &TextureID);
+     glDeleteVertexArrays(1, &VertexArrayID);
+
+     // Close OpenGL window and terminate GLFW
+     glfwTerminate();
+
+    return 0;
 }
+
--- common/controls.cpp
+++ common/controls.cpp
@@ -21,7 +21,8 @@ glm::mat4 getProjectionMatrix(){
 
 
 // Initial position : on +Z
-glm::vec3 position = glm::vec3( 0, 0, 5 ); 
+glm::vec3 position = glm::vec3( 0, 0, 10 );
+glm::vec3 rotation = glm::vec3( 0, 0, 0 );
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // Initial vertical angle : none
@@ -31,6 +32,7 @@ float initialFoV = 45.0f;
 
 float speed = 3.0f; // 3 units / second
 float mouseSpeed = 0.005f;
+float rad = 1.0f* 3.14f/180.0f;
 
 
 
@@ -48,12 +50,12 @@ void computeMatricesFromInputs(){
 	glfwGetCursorPos(window, &xpos, &ypos);
 
 	// Reset mouse position for next frame
-	glfwSetCursorPos(window, 1024/2, 768/2);
+	glfwSetCursorPos(window, 512/2, 512/2);
 
 	// Compute new orientation
-	horizontalAngle += mouseSpeed * float(1024/2 - xpos );
-	verticalAngle   += mouseSpeed * float( 768/2 - ypos );
-
+	horizontalAngle += mouseSpeed * float( 512/2 - xpos );
+	verticalAngle   += mouseSpeed * float( 512/2 - ypos );
+    
 	// Direction : Spherical coordinates to Cartesian coordinates conversion
 	glm::vec3 direction(
 		cos(verticalAngle) * sin(horizontalAngle), 
@@ -70,6 +72,32 @@ void computeMatricesFromInputs(){
 	
 	// Up vector
 	glm::vec3 up = glm::cross( right, direction );
+    
+    glm::mat3 rot_x =glm::mat3(
+                             glm::vec3(1.0f,  0.0f  ,   0.0f),
+                             glm::vec3(0.0f, cos(rad), -sin(rad)),
+                             glm::vec3(0.0f, sin(rad),  cos(rad))
+                             );
+   
+    glm::mat3 inv_rot_x =glm::mat3(
+                             glm::vec3(1.0f,  0.0f  ,   0.0f),
+                             glm::vec3(0.0f, cos(-rad), -sin(-rad)),
+                             glm::vec3(0.0f, sin(-rad),  cos(-rad))
+                             );
+   
+    glm::mat3 rot_y =glm::mat3(
+                             glm::vec3(cos(rad*deltaTime), 0.0f, sin(rad*deltaTime)),
+                             glm::vec3(0.0f,1.0f,0.0f),
+                             glm::vec3(-sin(rad*deltaTime), 0.0f, cos(rad*deltaTime))
+                             );
+    
+    glm::mat3 rot_z =glm::mat3(
+                             glm::vec3(cos(rad*deltaTime), -sin(rad*deltaTime), 0.0f),
+                             glm::vec3(-sin(rad*deltaTime), cos(rad*deltaTime), 0.0f),
+                             glm::vec3(0.0f, 0.0f, 1.0f)
+                             );
+    
+    
 
 	// Move forward
 	if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
@@ -87,15 +115,27 @@ void computeMatricesFromInputs(){
 	if (glfwGetKey( window, GLFW_KEY_LEFT ) == GLFW_PRESS){
 		position -= right * deltaTime * speed;
 	}
+    
+    if(glfwGetKey( window, GLFW_KEY_0) == GLFW_PRESS){
+        position = rot_x * position;
+        direction = rot_x * direction;
+        up = rot_x * up;
+    }
 
+    if(glfwGetKey( window, GLFW_KEY_1) == GLFW_PRESS){
+        position = inv_rot_x * position;
+        direction = inv_rot_x * direction;
+        up = inv_rot_x * up;
+    }
+    
 	float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+	ProjectionMatrix = glm::perspective(glm::radians(FoV), 3.0f / 3.0f, 0.1f, 100.0f);
 	// Camera matrix
 	ViewMatrix       = glm::lookAt(
-								position,           // Camera is here
-								position+direction, // and looks here : at the same position, plus "direction"
+                                position,           // Camera is here
+								position + direction, // and looks here : at the same position, plus "direction"
 								up                  // Head is up (set to 0,-1,0 to look upside-down)
 						   );
 
